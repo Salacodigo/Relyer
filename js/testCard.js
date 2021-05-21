d185408c615092dd785554c9b6fe6132f63a6fbd
JuanJoseF <juanjofr2002@gmail.com>
2021-05-21 18:58:45 -0500

    Feature 18.2 : Al cargar la pagina de preguntas de el test automaticamente se marcan las respuestas que se seleccionaron antes de salir la ultima vez

--- js/testCard.js
+++ js/testCard.js
@@ -3,6 +3,7 @@ let input = document.querySelectorAll('input');
 let cantidad_preguntas = 5;
 let preguntas_contestadas = 0;
 let barra_progreso = document.getElementsByClassName('barra')[0];
+window.onload = inicio();
 
 for (let i = 0; i < input.length; i++) {
     input[i].addEventListener('change', updateValue);
@@ -13,10 +14,26 @@ for (let i = 0; i < input.length; i++) {
     console.log(ancho);
 });*/
 
+function inicio() {
+    console.log('a');
+    let dataRespuestas = JSON.parse(localStorage.getItem("respuestas"));
+    if (dataRespuestas != null) {
+        alert('Se han cargado las respuestas que tenias antes de cerrar la pagina');
+
+        for (let i = 0; i < dataRespuestas.length; i++) {
+            if (dataRespuestas[i] != null) {
+                document.getElementById(dataRespuestas[i] + '.' + i).checked = true;
+            }
+        }
+    } else {
+        console.log('No hay respuestas marcadas');
+    }
+}
+
 function updateValue(e) {
     let dataRespuestas = JSON.parse(localStorage.getItem("respuestas"));
     console.log(e.srcElement.value + " " + e.srcElement.name);
-    if (dataRespuestas == null){
+    if (dataRespuestas == null) {
         console.log('a');
         localStorage.setItem('respuestas', JSON.stringify(respuestas));
         dataRespuestas = JSON.parse(localStorage.getItem("respuestas"));
@@ -29,25 +46,25 @@ function updateValue(e) {
             break;
         }
     }
-    for (let j = 0; j < cantidad_preguntas; j++){
-        if(dataRespuestas[j]!=null){
+    for (let j = 0; j < cantidad_preguntas; j++) {
+        if (dataRespuestas[j] != null) {
             preguntas_contestadas++;
         }
     }
     console.log(preguntas_contestadas);
-    if(preguntas_contestadas==1){
+    if (preguntas_contestadas == 1) {
         barra_progreso.style.width = '20%'
     }
-    else if(preguntas_contestadas==2){
+    else if (preguntas_contestadas == 2) {
         barra_progreso.style.width = '40%'
     }
-    else if(preguntas_contestadas==3){
+    else if (preguntas_contestadas == 3) {
         barra_progreso.style.width = '60%'
     }
-    else if(preguntas_contestadas==4){
+    else if (preguntas_contestadas == 4) {
         barra_progreso.style.width = '80%'
     }
-    else if(preguntas_contestadas==5){
+    else if (preguntas_contestadas == 5) {
         barra_progreso.style.width = '100%'
     }
 }
