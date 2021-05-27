8a4166039c3db14849c96d55d4b76c0ea77f02e5
tsweetheart <juan-daniel1914@hotmail.com>
2021-05-27 08:38:23 -0500

    Login and Register improve, fixing nav

--- js/homevideo.js
+++ js/homevideo.js
@@ -5,35 +5,35 @@ const navcontainer=document.getElementsByClassName('nav-container')[0];
 const playbutton=document.getElementsByClassName('play-button')[0];
 const home=document.getElementsByClassName('home')[0];
 
-playbutton.addEventListener('click',()=>{
+// playbutton.addEventListener('click',()=>{
 
-    videointro.setAttribute('style',
-    `
-    display:block;
-    animation: 3s rollvideo ease-in-out forwards;
-    position: absolute;
-    top: 0;
-    right: 0;
-    left: 0;
-    bottom: 0;
-    object-fit: cover;
-    width: 100%;
-    z-index: 100;`);
+//     videointro.setAttribute('style',
+//     `
+//     display:block;
+//     animation: 3s rollvideo ease-in-out forwards;
+//     position: absolute;
+//     top: 0;
+//     right: 0;
+//     left: 0;
+//     bottom: 0;
+//     object-fit: cover;
+//     width: 100%;
+//     z-index: 100;`);
     
-    videointro.play()
-    videointro.loop= true;
+//     videointro.play()
+//     videointro.loop= true;
     
-    navcontainer.setAttribute('style',`
-    background-color:rgba(14, 2, 2, 0.096);
-    `
-    )
+//     navcontainer.setAttribute('style',`
+//     background-color:rgba(14, 2, 2, 0.096);
+//     `
+//     )
 
 
-    home.setAttribute('style',`
-    opacity:0;
-    `)
+//     home.setAttribute('style',`
+//     opacity:0;
+//     `)
 
-})
+// })
 
 
 // document.addEventListener('DOMContentLoaded', ()=>{
