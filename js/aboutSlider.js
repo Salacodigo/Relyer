c0877f0951324ab26de8b0bb9c67a05833ff8b3d
tsweetheart <juan-daniel1914@hotmail.com>
2021-06-12 15:24:37 -0500

    first part aboutUs improved

---
+++ js/aboutSlider.js
@@ -0,0 +1,21 @@
+
+    const swiper = new Swiper('.swiper-container', {
+
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+
+        // If we need pagination
+        pagination: {
+            el: '.swiper-pagination',
+            dynamicBullets: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+
+    });
