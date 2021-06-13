3cae5d4b20dc27a07ca6ac22edbf292958fd8e17
tsweetheart <juan-daniel1914@hotmail.com>
2021-06-13 11:44:12 -0500

    aboutUs part 2

--- js/aboutSlider.js
+++ js/aboutSlider.js
@@ -19,3 +19,25 @@
         },
 
     });
+
+    
+    const swiper2 = new Swiper('.swiper-container2', {
+
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        speed: 500,
+
+        // If we need pagination
+        pagination: {
+            el: '#pagination2',
+            dynamicBullets: true,
+        },
+
+        // Navigation arrows
+        navigation: {
+            nextEl: '#buttonnext2',
+            prevEl: '#buttonprev2',
+        },
+
+    });
