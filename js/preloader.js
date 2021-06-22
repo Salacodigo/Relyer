0a932dff4522222137891470fa8e0728b59e0b4f
Alejandro <alejandrovillamilcarmona@gmail.com>
2021-06-21 19:22:27 -0500

    cambios

---
+++ js/preloader.js
@@ -0,0 +1,7 @@
+function preloader() {
+    var preloader = document.getElementById("preloader");
+    preloader.style.opacity = "0";
+    preloader.setAttribute("aria-busy", "false");
+    document.getElementById("site").style.opacity = "1";
+  }
+  window.onload = preloader;
