520cd6fb62764a996c372a87acc5d46a3b13632a
tsweetheart <juan-daniel1914@hotmail.com>
2021-07-23 22:37:01 -0500

    slider tools and experts

--- js/expertsandtools.js
+++ js/expertsandtools.js
@@ -1,19 +1,5 @@
 
-!async function fetchTool() {
-
-    let request = await fetch('https://server-relyer.herokuapp.com/api/tools')
-    let response = await request.json()
-    let tools = response.tools
-
-    console.log('%c tools', 'color:blue;')
-    console.log(tools)
-
-    tools.forEach(tool => {
-        console.log(tool)
-    });
-
-
-}()
+const expertsContainer = document.getElementsByClassName('swiper-wrapper')[0]
 
 !async function fetchExpert() {
 
@@ -25,7 +11,38 @@
     console.log(experts)
 
     experts.forEach(expert => {
-        console.log(expert)
+
+        let swiperSlide = document.createElement('div')
+        swiperSlide.className = "swiper-slide"
+
+        swiperSlide.innerHTML = `
+        <div class="expert-card">
+            <img src="https://source.unsplash.com/140x140/?girl" alt="girl">
+            <h2>${expert.nombre}</h2>
+            <p>${expert.experience}</p>
+            <div class="category">
+                <h2>${expert.category}</h2>
+            </div>
+            <button>Contact Me</button>
+        </div>`
+
+        expertsContainer.appendChild(swiperSlide)
+    });
+
+
+}()
+
+
+!async function fetchTool() {
+
+    let request = await fetch('https://server-relyer.herokuapp.com/api/tools')
+    let response = await request.json()
+    let tools = response.tools
+
+    console.log('%c tools', 'color:blue;')
+    console.log(tools)
+
+    tools.forEach(tool => {
     });
 
 
