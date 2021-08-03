408de47c93f9534e408f3d042a2e085f99b9c362
Alejandro <alejandrovillamilcarmona@gmail.com>
2021-08-02 19:08:10 -0500

    terminar test

--- js/testInformative.js
+++ js/testInformative.js
@@ -1,21 +1,13 @@
 let cards = document.querySelectorAll('.card');
-console.log(cards)
 let tiempo = 1000;
 
-function xxxx (){
-     
-    
+function cardsUp (){
     for (let i = 0; i<cards.length ; i++) {
         setTimeout( () =>{ cards[i].classList.toggle('arriba') } , tiempo);tiempo = tiempo + 1000;
     
+    }
 }
-}
-    
-  
-        
-        setInterval(() => {
-            xxxx();
-        } , 50);
+setInterval(() => cardsUp() , 5);
         
         
                 
